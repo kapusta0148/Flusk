75307050e478e35a52175df311aa60217169c794
commandor <nikolaev.dme@gmail.com>
2025-03-09 20:24:12 +0300

    SOL 1

---
+++ WEB_Введение_во_flask_Обработка_HTML-форм/Изображение_Марса.py
@@ -0,0 +1,35 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def mission_name():
+    return 'Миссия Колонизация Марса'
+
+
+@app.route('/index')
+def mission_motto():
+    return 'И на Марсе будут яблони цвести!'
+
+
+@app.route('/promotion')
+def promotion():
+    text = ['Человечество вырастает из детства.',
+            'Человечеству мала одна планета.',
+            'Мы сделаем обитаемыми безжизненные пока планеты.',
+            'И начнем с Марса!',
+            'Присоединяйся!']
+    return '</br>'.join(text)
+
+
+@app.route('/image_mars')
+def image_mars():
+    with open("Mars.html", "r") as html:
+        html = html.read()
+    return html
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080)
