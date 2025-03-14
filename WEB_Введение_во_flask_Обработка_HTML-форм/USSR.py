56c369f5f80e543e601267ae5113dcfacfcf0a80
commandor <nikolaev.dme@gmail.com>
2025-03-15 00:22:18 +0300

    4 zadacha

---
+++ WEB_Введение_во_flask_Обработка_HTML-форм/USSR.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+@app.route('/choice/<planet_name>')
+def choice(planet_name):
+    return render_template("porabotim_mars.html", planet=planet_name)
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080)
+    print(choice)
