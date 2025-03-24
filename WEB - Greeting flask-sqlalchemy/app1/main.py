dce8c50e6f38066ef673aae22c5334948b40d754
commandor <nikolaev.dme@gmail.com>
2025-03-24 20:37:23 +0300

    Bash

--- WEB - Greeting flask-sqlalchemy/app1/main.py
+++ WEB - Greeting flask-sqlalchemy/app1/main.py
@@ -1,6 +1,6 @@
 from flask import Flask
-
-from data.news import News
+import datetime
+from data.jobs import Jobs
 from data.users import User
 from data import db_session
 
@@ -18,7 +18,15 @@ def main():
     user.speciality = "Italian_WW2_plane"
     user.address = "Italy_Venice"
     user.email = "BomBombini@gusine.com"
+    job = Jobs()
+    job.team_leader = 1
+    job.job = "deployment of residential modules 1 and 2"
+    job.work_size = 15
+    job.collaborators = "2, 3"
+    job.start_date = datetime.datetime.now()
+    job.is_finished = False
     db_sess = db_session.create_session()
+    db_sess.add(job)
     db_sess.add(user)
     db_sess.commit()
 
--- WEB_Rabota_s_komandnoyy_strokoy/Bash-razminka.py
+++ WEB_Rabota_s_komandnoyy_strokoy/Bash-razminka.py
@@ -1,9 +1,15 @@
-import sys
+import schedule
+import time
+from datetime import datetime
 
-try:
-    if len(sys.argv) != 3:
-        print(0)
-    else:
-        print(int(sys.argv[1]) + int(sys.argv[2]))
-except ValueError:
-    print(0)
+def ku_job():
+    current_hour = datetime.now().hour
+    repeat_count = current_hour % 12
+    repeat_count = 12 if repeat_count == 0 else repeat_count
+    print("Ку " * repeat_count)
+
+schedule.every().hour.at(":00").do(ku_job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
