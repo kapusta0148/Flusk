0ead48f2494b86680f200255144a6748a1390925
commandor <nikolaev.dme@gmail.com>
2025-05-10 18:07:45 +0300

    new WEB

--- WEB_Flask-sqlalchemy/Model_formy_avtorizatsii_polzovatelya/main.py
+++ WEB_Flask-sqlalchemy/Model_formy_avtorizatsii_polzovatelya/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, abort
 from data import db_session
 from data.users import User
 from data.jobs import Jobs
@@ -13,6 +13,63 @@ db_session.global_init("db/mars_users.sqlite")
 
 login_manager = LoginManager()
 login_manager.init_app(app)
+
+
+@app.route('/jobs/<int:id>/delete', methods=['POST', 'GET'])
+@login_required
+def delete_job(id):
+    db_sess = db_session.create_session()
+    job = db_sess.query(Jobs).get(id)
+
+    if not job:
+        abort(404)
+
+    if job.team_leader != current_user.id and current_user.id != 1:
+        abort(403)
+
+    db_sess.delete(job)
+    db_sess.commit()
+    return redirect('/')
+
+
+
+@app.route('/jobs/<int:id>/edit', methods=['GET', 'POST'])
+@login_required
+def edit_job(id):
+    form = JobForm()
+    db_sess = db_session.create_session()
+    job = db_sess.query(Jobs).get(id)
+
+    if not job:
+        abort(404)
+
+    if current_user.id != 1 and job.team_leader != current_user.id:
+        abort(403)
+
+    if form.validate_on_submit():
+        if current_user.id == 1:
+            job.team_leader = form.team_leader.data
+        else:
+            job.team_leader = current_user.id
+
+        job.job = form.job.data
+        job.work_size = form.work_size.data
+        job.collaborators = form.collaborators.data
+        job.start_date = form.start_date.data
+        job.is_finished = form.is_finished.data
+
+        db_sess.commit()
+        return redirect('/')
+
+    form.job.data = job.job
+    form.work_size.data = job.work_size
+    form.collaborators.data = job.collaborators
+    form.start_date.data = job.start_date
+    form.is_finished.data = job.is_finished
+    if current_user.id == 1:
+        form.team_leader.data = job.team_leader
+
+    return render_template('add_job.html', form=form, title='Редактировать работу')
 
 
 @app.route('/add_job', methods=['GET', 'POST'])
